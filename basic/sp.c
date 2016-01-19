4178bed7ca58863e0d4b9d7ae04e317045511cd7
Andrew Santosa <dcsandr@nus.edu.sg>
2016-01-19 16:31:28 +0800

    Added sp.c for testing subsumption due to strongest postcondition

---
+++ basic/sp.c
@@ -0,0 +1,44 @@
+/*
+Copyright 2015 National University of Singapore 
+cd
+cd nus/kleetest
+llvm-gcc --emit-llvm -c -g sp.c
+llvm-gcc -S --emit-llvm sp.c
+opt -analyze -dot-cfg sp.o
+klee -write-pcs -use-query-log=all:pc,all:smt2 -search=dfs sp.o
+ktest-tool --write-ints klee-last/test00000?.ktest
+
+*/
+#include <klee/klee.h>
+#include <assert.h>
+
+int y;
+
+int proc(int x, int y) { 
+  if (x > 0) {
+    x = 2;
+  } else {
+    x += 2;
+  }
+
+  if (y > 0) {
+    y = 2;
+    x++;
+  } else {
+    y += 2;
+    x++;
+  }
+
+  assert (x + y <= 5);
+
+  return x + y;
+}
+
+int main() {
+  int x, y;
+
+  klee_make_symbolic(&x, sizeof(x), "x");
+  klee_make_symbolic(&y, sizeof(y), "y");
+
+  return proc(x, y);
+} 
