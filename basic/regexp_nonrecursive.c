bdefc785cb56f0a63c166240d35aff00689c3ed5
Andrew Santosa <dcsandr@nus.edu.sg>
2016-03-10 10:49:32 +0800

    Added small regexp in basic/ subdirectory, and fixed typo in comment of scalability/Regexp.c

--- basic/regexp_nonrecursive.c
+++ basic/regexp_nonrecursive.c
@@ -8,6 +8,9 @@
  * This code with KLEE harness is obtained from the following
  * page of the KLEE tutorial on December 2015:
  *   http://klee.github.io/resources/Regexp.c.html
+ *
+ * We disabled recursive calls so that the search space is
+ * small
  */ 
 
 #include <klee/klee.h>
@@ -24,13 +27,15 @@ static int matchstar(int c, char *re, char *text) {
 
 static int matchhere(char *re, char *text) {
   if (re[0] == '\0')
-     return 0;
+    return 0;
   if (re[1] == '*')
-    return matchstar(re[0], re+2, text);
+    /* return matchstar(re[0], re+2, text); */
+    return 0;
   if (re[0] == '$' && re[1]=='\0')
     return *text == '\0';
   if (*text!='\0' && (re[0]=='.' || re[0]==*text))
-    return matchhere(re+1, text+1);
+    /* return matchhere(re+1, text+1); */
+    return 0;
   return 0;
 }
 
