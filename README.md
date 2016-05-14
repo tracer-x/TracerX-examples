e1b8b335df3bd6fc36593fbd69c15c801586a605
Andrew Santosa <santosa_1999@yahoo.com>
2016-05-14 08:13:26 +0800

    Polished the comments and properly indent the programs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # grading-examples
 
-Simple examples for running with Tracer-X/ KLEE.
+Copyright 2016 National University of Singapore
 
-It currently contains academic program and 
-some examples taken from [KLEE OSDI paper 2008] (https://www.doc.ic.ac.uk/~cristic/papers/klee-osdi-08.pdf).
+Simple examples for running with KLEE. This repository contains
+academic programs, with an example taken from [KLEE OSDI paper
+2008] (https://www.doc.ic.ac.uk/~cristic/papers/klee-osdi-08.pdf).
--- mod_eq.c
+++ mod_eq.c
@@ -1,33 +1,37 @@
 /*
-make clean
-clang -emit-llvm -g -c mod_eq.c
-time /home/felicia/git/tracerx/klee/Release+Asserts/bin/klee -max-time=10 -output-tree -interpolation-stat -write-pcs -use-query-log=all:pc,all:smt2 -allow-external-sym-calls -search=dfs -select-solver=stp -output-dir=mod_eq.stpklee mod_eq.bc -max-fail 1
+ * From Figure 11 in Cadar, Dunbar, Engler: KLEE: Unassisted and
+ * Automatic Generation of High-Coverage Tests for Complex Systems
+ * Programs, OSDI '08.
+ * 
+ * make clean
+ * clang -emit-llvm -g -c mod_eq.c
+ * time /home/felicia/git/tracerx/klee/Release+Asserts/bin/klee -max-time=10 -output-tree -interpolation-stat -write-pcs -use-query-log=all:pc,all:smt2 -allow-external-sym-calls -search=dfs -select-solver=stp -output-dir=mod_eq.stpklee mod_eq.bc -max-fail 1
+ */
 
-*/
 #include <klee/klee.h>
 #include<stdio.h>
 #include <assert.h>
 
 unsigned mod_opt(unsigned x, unsigned y) {
-
-	if((y & -y) == y) { 
-		return x & (y-1);
-	}
-	else{
-		return x % y;
-	}
+  if((y & -y) == y) { 
+    return x & (y-1);
+  }
+  else{
+    return x % y;
+  }
 }
+
 unsigned mod(unsigned x, unsigned y) {
-	 return x % y;
+  return x % y;
 }
 
 int main() {
- unsigned x,y;
-
- klee_make_symbolic(&x, sizeof(x), "x");
- klee_make_symbolic(&y, sizeof(y), "y");
- klee_assume(x <= 10);
- klee_assume(y <= 10);
- assert(mod(x,y) == mod_opt(x,y));
- return 0;
+  unsigned x,y;
+  
+  klee_make_symbolic(&x, sizeof(x), "x");
+  klee_make_symbolic(&y, sizeof(y), "y");
+  klee_assume(x <= 10);
+  klee_assume(y <= 10);
+  assert(mod(x,y) == mod_opt(x,y));
+  return 0;
 }
