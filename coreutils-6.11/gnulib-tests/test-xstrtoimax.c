3488e02f12c990a9752361c4a4b38fa4ba378e58
felicia.halim <felicia.halim@live.com>
2016-06-14 16:16:12 +0800

    Remove coreutils-6.11 because this time we only use coreutils 6.10

--- coreutils-6.11/gnulib-tests/test-xstrtoimax.c
+++
@@ -1,7 +0,0 @@
-/* -*- buffer-read-only: t -*- vi: set ro: */
-/* DO NOT EDIT! GENERATED AUTOMATICALLY! */
-#line 1
-#define __xstrtol xstrtoimax
-#define __strtol_t intmax_t
-#define __spec PRIdMAX
-#include "test-xstrtol.c"
