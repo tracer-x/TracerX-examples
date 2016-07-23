c4974962828a325acafc579b39c8b160e86d40c6
Andrew Santosa <santosa_1999@yahoo.com>
2016-07-23 08:21:28 +0800

    Fixed join/sum.c example

--- join/sum.c
+++ join/sum.c
@@ -1,26 +1,25 @@
 /*
  * Copyright 2016 National University of Singapore
- *
- * Author: Joxan Jaffar
  */
 #include <klee/klee.h>
 #include <assert.h>
 #include <stdio.h>
+
 #define SIZE 3
 
 int main() {
   char a[SIZE];
-  unsigned i, sum1, sum2;
+  unsigned i, sum1 = 0, sum2 = 0;
 
   klee_make_symbolic(a, SIZE * sizeof(char), "a");
 
   for (i = 0; i < SIZE; ++i) {
-    if(a[i] > 0) sum1 += a[i];
+    if (a[i] > 0) sum1 += a[i];
   }
 
   for (i = 0; i < SIZE; ++i) {
     sum2 += a[i];
-    klee_join("sum", a, sum2);
+    klee_join("sum", a, i, sum2);
   }
 
   assert(sum1 == sum2); // The assertion should be violated because sum1 only sum positive numbers, while sum2 sum all the numbers.
