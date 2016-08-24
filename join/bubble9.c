20d763ad94e8397f1c404795a5ebcf34b5fe72f6
Andrew Santosa <dcsandr@nus.edu.sg>
2016-08-24 15:56:10 +0800

    Added bubble sort of 9 elements in join subdirectory

---
+++ join/bubble9.c
@@ -0,0 +1,47 @@
+/*
+ * Bubble sort
+ * 
+ * From
+ * http://www.programmingsimplified.com/c/source-code/c-program-bubble-sort
+ *
+ * This is an example whose analysis by naive path exploration is
+ * expensive. Note that the paper "Sorting nine inputs requires
+ * twenty-five comparisons" by Codish et
+ * al. http://arxiv.org/abs/1405.5754 suggests that in standard
+ * sorting algorithm there can possibly be plenty of superfluous
+ * comparisons.
+ */
+#include <klee/klee.h>
+#include <assert.h>
+
+#define ARRAY_SIZE 9
+
+int main() {
+  int a[ARRAY_SIZE];
+  int n, c, d, swap;
+ 
+  klee_make_symbolic(a, ARRAY_SIZE * sizeof(int), "a");
+
+  for (c = 0 ; c < ( ARRAY_SIZE - 1 ); c++)
+  {
+    for (d = 0 ; d < ARRAY_SIZE - c - 1; d++)
+    {
+      if (a[d] > a[d+1]) /* For decreasing order use < */
+      {
+        swap       = a[d];
+        a[d]   = a[d+1];
+        a[d+1] = swap;
+      }
+    }
+    
+    klee_join("bubble_max", a, ARRAY_SIZE - c);
+  }
+
+  assert (a[0] <= a[1] &&
+	  a[1] <= a[2] &&
+	  a[2] <= a[3] &&
+	  a[3] <= a[4] &&
+	  a[4] <= a[5] &&
+	  a[6] <= a[7] &&
+	  a[7] <= a[8]);
+}
