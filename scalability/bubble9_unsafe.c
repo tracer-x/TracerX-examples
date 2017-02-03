3dd7797a2ce988df390aa0c0869cfcdd7ccd78be
Andrew Santosa <dcsandr@nus.edu.sg>
2017-02-03 11:35:28 +0800

    Added LLBMC harnessing to bubble9_unsafe.c

--- scalability/bubble9_unsafe.c
+++ scalability/bubble9_unsafe.c
@@ -1,6 +1,6 @@
 /*
  * Bubble sort
- * 
+ *
  * From
  * http://www.programmingsimplified.com/c/source-code/c-program-bubble-sort
  *
@@ -14,16 +14,25 @@
  * This is a version with a bug introduced where we re-swap the last
  * two elements.
  */
+#ifdef LLBMC
+#include <llbmc.h>
+#else
 #include <klee/klee.h>
-#include <assert.h>
+#endif
 
 #define ARRAY_SIZE 9
 
 int main() {
   int a[ARRAY_SIZE];
   int n, c, d, swap;
- 
+
+#ifdef LLBMC
+  for (int i = 0; i < ARRAY_SIZE ; ++i) {
+    a[i] = __llbmc_nondef_int();
+  }
+#else
   klee_make_symbolic(a, ARRAY_SIZE * sizeof(int), "a");
+#endif
 
   for (c = 0 ; c < ( ARRAY_SIZE - 1 ); c++)
   {
@@ -42,11 +51,21 @@ int main() {
   a[7] = a[8];
   a[8] = tmp;
 
-  assert (a[0] <= a[1] &&
-	  a[1] <= a[2] &&
-	  a[2] <= a[3] &&
-	  a[3] <= a[4] &&
-	  a[4] <= a[5] &&
-	  a[6] <= a[7] &&
-	  a[7] <= a[8]);
+#ifdef LLBMC
+  __llbmc_assert(a[0] <= a[1] && \
+		 a[1] <= a[2] && \
+		 a[2] <= a[3] && \
+		 a[3] <= a[4] && \
+		 a[4] <= a[5] && \
+		 a[6] <= a[7] && \
+		 a[7] <= a[8]);
+#else
+  klee_assert(a[0] <= a[1] && \
+	      a[1] <= a[2] && \
+	      a[2] <= a[3] && \
+	      a[3] <= a[4] && \
+	      a[4] <= a[5] && \
+	      a[6] <= a[7] && \
+	      a[7] <= a[8]);
+#endif
 }
