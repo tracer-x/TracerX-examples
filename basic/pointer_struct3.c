b2123e94658a0e44f97c94ff787499791c8b3563
Andrew Santosa <dcsandr@nus.edu.sg>
2017-03-17 13:42:18 +0800

    Updated basic/pointer_struct3.c

--- basic/pointer_struct3.c
+++ basic/pointer_struct3.c
@@ -1,5 +1,8 @@
 /*
- * Subsumption of dynamically-allocated memory.
+ * Subsumption of dynamically-allocated memory. Although make_node is
+ * executed in different paths and should result in different base
+ * allocation address, subsumption should still occur as we are using
+ * abstract address for comparison.
  *
  * Copyright 2017 National University of Singapore
  */
@@ -11,10 +14,10 @@ struct Node {
   struct Node *next;
 };
 
-struct Node *make_node() {
+struct Node *make_node(int x) {
   struct Node *n = (struct Node *) malloc(sizeof(struct Node));
 
-  n->data = 32;
+  n->data = 32 + x;
   n->next = NULL;
 
   return n;
@@ -29,9 +32,9 @@ int main(int argc, char **argv) {
   klee_make_symbolic(&p2, sizeof(p2), "p2");
   
   if (p1) {
-    p = make_node();
+    p = make_node(1);
   } else {
-    p = make_node();
+    p = make_node(2);
   }
 
   if (p2) {
@@ -40,8 +43,5 @@ int main(int argc, char **argv) {
     x += 2;
   }
 
-  if (p->data == 32)
-    return 0;
-  
-  return 1;
+  return (int) p->data;
 }
