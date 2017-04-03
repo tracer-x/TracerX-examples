9c55627efc9a5de5f87269d98443bdab1d52c459
Andrew Santosa <dcsandr@nus.edu.sg>
2017-04-03 17:37:58 +0800

    Added LLBMC API for basic/malloc2.c and basic/pointer_struct7.c

--- basic/malloc2.c
+++ basic/malloc2.c
@@ -3,9 +3,12 @@
  *
  * Copyright 2017 National University of Singapore
  */
-
+#ifdef LLBMC
+#include <llbmc.h>
+#else
 #include <klee/klee.h>
 #include <assert.h>
+#endif
 #include <stdlib.h>
 
 #define MAX 9
@@ -16,17 +19,26 @@ int main(int argc, char **argv) {
   int *x, *y, z, i;
   x = malloc(sizeof(int));
 
+#ifdef LLBMC
+  n = __llbmc_nondef_int();
+  __llbmc_assume(n <= 0);
+#else
   klee_make_symbolic(&n, sizeof(int), "n");
   klee_assume(n <= 0);
-  
+#endif
+
   *x = n; 
   for (i = 1; i < MAX; i++) {
     char nondet;
     
     y = malloc(sizeof(int));
 
+#ifdef LLBMC
+    nondet = __llbmc_nondef_char();
+#else
     klee_make_symbolic(&nondet, sizeof(char), "nondet");
-    
+#endif
+
     if (nondet) {
       z += 1;
       *y = *x + 10;
@@ -37,8 +49,12 @@ int main(int argc, char **argv) {
     
     x = y;
   }
-  
+
+#ifdef LLBMC
+  __llbmc_assert(*x < n + 999);
+#else
   klee_assert(*x < n + 999);
+#endif
 
   return z;
 }
--- basic/pointer_struct7.c
+++ basic/pointer_struct7.c
@@ -4,8 +4,12 @@
  * Copyright 2017 National University of Singapore
  */
 
+#ifdef LLBMC
+#include <llbmc.h>
+#else
 #include <klee/klee.h>
 #include <assert.h>
+#endif
 #include <stdlib.h>
 
 #define MAX 9
@@ -21,15 +25,24 @@ int main(int argc, char **argv) {
   struct node *x, *y;
   int z, i;
 
+#ifdef LLBMC
+  n = __llbmc_nondef_int();
+  __llbmc_assume(n <= 0);
+#else
   klee_make_symbolic(&n, sizeof(int), "n");
   klee_assume(n <= 0);
- 
+#endif
+
   x = malloc(sizeof(struct node));
   x->val = n;
   for (i = 1; i < MAX; i++) {
     char nondet;
 
+#ifdef LLBMC
+    nondet = __llbmc_nondef_char();
+#else
     klee_make_symbolic(&nondet, sizeof(char), "nondet");
+#endif
 
     x->next = y = malloc(sizeof(struct node));
     if (nondet) {
@@ -43,7 +56,11 @@ int main(int argc, char **argv) {
     x = y;
   }
 
+#ifdef LLBMC
+  __llbmc_assert(x->val < n + 999);
+#else
   klee_assert(x->val < n + 999);
+#endif
 
   return z;
 }
