24d7be0524e2aab7bdd14aae5a2253bca28788f5
Andrew Santosa <dcsandr@nus.edu.sg>
2017-05-14 14:55:41 +0800

    Updated utils/fmcad17-scalability.sh

--- utils/fmcad17-scalability.sh
+++ utils/fmcad17-scalability.sh
@@ -13,9 +13,8 @@ unset CPLUS_INCLUDE_PATH
 
 ( \
     cd $SCALABILITY_DIR ; \
-    export EXPERIMENT_SET="malloc9 malloc18 malloc27 bubble_assert6 bubble_assert7 bubble_assert8 Regexp-6-4 Regexp-7-4 Regexp-8-4 insertsort7 insertsort9 insertsort11 statemate nsichneu" ; \
-    export EXPERIMENT_TYPE_SET=".klee1 .tx1 .tx2" ; \
+    export EXPERIMENT_SET="malloc9 malloc18 malloc27 insertsort5 insertsort7 insertsort9 insertsort11 insertsort_assert5 insertsort_assert7 insertsort_assert9 insertsort_assert11 bubble3 bubble4 bubble5 bubble6 bubble7 bubble8 bubble9 bubble12 bubble_assert3 bubble_assert4 bubble_assert5 bubble_assert6 bubble_assert7 bubble_assert8 bubble_assert9 bubble_assert12 Regexp-6-3 Regexp-6-4 Regexp-6-5 Regexp-7-3 Regexp-7-4 Regexp-7-5 Regexp-7-6 Regexp-8-3 Regexp-8-4 Regexp-8-5 nsichneu statemate" ; \
+    export EXPERIMENT_TYPE_SET=".klee1 .tx1 .tx2a" ; \
     make experiment.csv ; \
     make llbmc-experiment.csv ; \
 )
-
