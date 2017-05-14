12d4d3f7a9c40733b6084ac3ee0d2aa13fa5d643
Andrew Santosa <dcsandr@nus.edu.sg>
2017-05-14 14:03:08 +0800

    Added scalability/bubble[number].c of bubble sorts without sortedness assertion checks

---
+++ scalability/bubble6.c
@@ -0,0 +1,48 @@
+/*
+ * Bubble sort
+ * 
+ * From
+ * http://www.programmingsimplified.com/c/source-code/c-program-bubble-sort
+ *
+ * This is an example whose analysis by naive path exploration is
+ * expensive. Note that the paper "Sorting nine inputs requires
+ * twenty-five comparisons" by Codish et
+ * al. http://arxiv.org/abs/1405.5754 suggests that in standard
+ * sorting algorithm there can possibly be plenty of superfluous
+ * comparisons.
+ */
+#ifdef LLBMC
+#include <llbmc.h>
+#else
+#include <assert.h>
+#include <klee/klee.h>
+#endif
+
+#define ARRAY_SIZE 6
+
+int main() {
+  int a[ARRAY_SIZE];
+  int n, c, d, swap;
+
+#ifdef LLBMC
+  for (int i = 0; i < ARRAY_SIZE ; ++i) {
+    a[i] = __llbmc_nondef_int();
+  }
+#else
+  klee_make_symbolic(a, ARRAY_SIZE * sizeof(int), "a");
+#endif
+
+  for (c = 0 ; c < ( ARRAY_SIZE - 1 ); c++)
+  {
+    for (d = 0 ; d < ARRAY_SIZE - c - 1; d++)
+    {
+      if (a[d] > a[d+1]) /* For decreasing order use < */
+      {
+        swap       = a[d];
+        a[d]   = a[d+1];
+        a[d+1] = swap;
+      }
+    }
+  }
+  return 0;
+}
