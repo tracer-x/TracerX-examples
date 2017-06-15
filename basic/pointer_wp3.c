667fc07a1bd1c4caba0ddb3a6f492f74f5c64d84
Andrew Santosa <dcsandr@nus.edu.sg>
2017-06-15 12:13:21 +0800

    Added basic/pointer_wp2.c and basic/pointer_wp3.c

---
+++ basic/pointer_wp3.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016, 2017 National University of Singapore
+ *
+ * This program is for testing memory bounds check interpolation: it
+ * should have subsumptions not just at/near the end of the traces.
+ */
+#ifdef LLBMC
+#include <llbmc.h>
+#else
+#include <klee/klee.h>
+#endif
+
+int tracerx_check(char *p) {
+  return *p;
+}
+
+int main(int argc, char **argv)
+{
+  char s[10];
+  int p1, p2, p3;
+  char *p;
+
+#ifdef LLBMC
+  p1 = __llbmc_nondef_int();
+  p2 = __llbmc_nondef_int();
+  p3 = __llbmc_nondef_int();
+#else
+  klee_make_symbolic(&p1, sizeof(int), "p1");
+  klee_make_symbolic(&p2, sizeof(int), "p2");
+  klee_make_symbolic(&p3, sizeof(int), "p3");
+#endif
+
+  p = s;
+
+  if (p1) {
+    p += 2;
+  } else {
+    p += 1;
+  }
+  if (p2) {
+    p += 2;
+  } else {
+    p += 1;
+  }
+  if (p3) {
+    p += 2;
+  } else {
+    p += 7;
+  }
+
+  // p should be in s
+  return tracerx_check(p);
+}
+
