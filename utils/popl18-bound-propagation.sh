6b6adf269be4370ef0f0dc82f77f0dfdbb7abc7f
rasoolmaghareh <rasool@comp.nus.edu.sg>
2017-06-15 16:12:35 +0800

    adding the script to run WCET experiments + cnt benchmark

---
+++ utils/popl18-bound-propagation.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+#
+# WCET experiments for POPL '18 submission. 
+#
+# Copyright 2017 National University of Singapore
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $SCRIPT_DIR/../environ.sh
+WCET_DIR=$SCRIPT_DIR/../binary-chop
+
+unset C_INCLUDE_PATH
+unset CPLUS_INCLUDE_PATH
+
+red='\e[1;31m%s\e[0m\n'
+green='\e[1;32m%s\e[0m\n'
+yellow='\e[1;33m%s\e[0m\n'
+blue='\e[1;34m%s\e[0m\n'
+magenta='\e[1;35m%s\e[0m\n'
+cyan='\e[1;36m%s\e[0m\n'
+
+export EXPERIMENT_SET="cnt"  # statemate_w_bound cnt_w_bound Regexp7_w_bound
+export EXPERIMENT_TYPE_SET=".tx1"
+export ENABLE_COVERAGE=OFF
+
+# Adding the target in benchmarks
+export BENCHMARKS=$(echo $EXPERIMENT_SET | tr " " "\n")
+
+for BENCHMARK in $BENCHMARKS
+do
+	export UPPER_BOUND=1000
+	export CURRENT_BOUND=$UPPER_BOUND
+	export LOWER_BOUND=1
+        printf "\n\nAnalysing $BENCHMARK:\n" 
+	while true; do
+	        echo "Adding bound $BOUND to $BENCHMARK." 
+		echo "UPPER BOUND: "$UPPER_BOUND
+	        echo "CURRENT BOUND: "$CURRENT_BOUND
+	        echo "LOWER BOUND: "$LOWER_BOUND
+		echo "Precision: " 
+		echo "scale = 2;100*(1-($UPPER_BOUND-$LOWER_BOUND)/sqrt(($UPPER_BOUND^2+$LOWER_BOUND^2)/2));scale=2" | bc -l
+		cat src/"$BENCHMARK".pre > "$BENCHMARK".c 
+		echo "char _bound[$CURRENT_BOUND];"  >> "$BENCHMARK".c  
+		cat src/"$BENCHMARK".post >> "$BENCHMARK".c   
+		make "$BENCHMARK".tx 2> output.txt > output.txt 
+		printf "Instructions Count: "
+		klee-stats "$BENCHMARK".tx | sed '4q;d' | cut -d '|' -f3  | tr -d '\n' | tr -d ' ' 
+		printf "\nAnalysis time: "
+		klee-stats "$BENCHMARK".tx | sed '4q;d' | cut -d '|' -f4  | tr -d '\n' | tr -d ' ' 
+		if ! grep -qF "ERROR:" "$BENCHMARK".tx/messages.txt; then
+		    printf "\n$green\n" "Bound is safe"	
+		    echo
+		    UPPER_BOUND=$[$CURRENT_BOUND]
+		    if  [ $(echo " ($UPPER_BOUND-$LOWER_BOUND)/sqrt(($UPPER_BOUND^2+$LOWER_BOUND^2)/2) <= 0.01" | bc -l) -eq 1 ] || [ "$UPPER_BOUND" -eq 1 ] ; then
+	  		echo
+		       	echo "Analysis has finished:"
+		        echo "UPPER BOUND: "$UPPER_BOUND
+		        echo "LOWER BOUND: "$LOWER_BOUND
+			echo "Precision: " 
+			echo "scale = 2;100*(1-($UPPER_BOUND-$LOWER_BOUND)/sqrt(($UPPER_BOUND^2+$LOWER_BOUND^2)/2));scale=2" | bc -l
+			rm -R "$BENCHMARK".tx
+			rm "$BENCHMARK".c
+			rm output.txt
+		        break
+		    else
+  		        CURRENT_BOUND=$[($UPPER_BOUND + $LOWER_BOUND) / 2]
+		    fi
+		else
+		    COUNTER_EXAMPLE=$(grep 'Timing of Path:[0-9][0-9]*' output.txt | tail -1)
+		    printf "\nCounter Example Time:"
+		    echo $[${COUNTER_EXAMPLE:15}]
+		    printf "$red\n" "Bound is not safe"
+		    echo	
+  		    LOWER_BOUND=$[${COUNTER_EXAMPLE:15} + 1]
+  		    CURRENT_BOUND=$[($UPPER_BOUND + $LOWER_BOUND) / 2]
+		fi	
+		rm -R "$BENCHMARK".tx
+		rm output.txt
+	done
+done
+
+
+#( \
+#    cd $WCET_DIR ; \
+#    make experiment.csv ; \
+#)
+
+#export EXPERIMENT_SET="Regexp7 Regexp7_w_bound Regexp8 Regexp8_w_bound RegexpSize9 RegexpSize9_w_bound RegexpSize10 RegexpSize10_w_bound RegexpSize15 RegexpSize15_w_bound"
+#export EXPERIMENT_TYPE_SET=".klee1 .tx1"
+#export EXPERIMENT_TYPE_SET=".tx1"
+
+
+#( \
+#    cd $WCET_DIR ; \
+#    make experiment.csv  ENABLE_COVERAGE=OFF; \
+#    make llbmc-experiment.csv ; \
+#    mv llbmc-experiment.csv llbmc-experiment.csv1 ; \
+#    mv experiment.csv experiment.csv1 ; \
+#)
+
+
+#export EXPERIMENT_SET="statemate statemate_w_bound nsichneu nsichneu_w_bound"
+
+#( \
+#    cd $WCET_DIR ; \
+#    make experiment.csv  ENABLE_COVERAGE=OFF; \
+#    make llbmc-experiment.csv ; \
+#    cat llbmc-experiment.csv0 >> llbmc-experiment.csv ; \
+#    cat llbmc-experiment.csv1 >> llbmc-experiment.csv ; \
+#    cat experiment.csv0 >> experiment.csv ; \
+#    cat experiment.csv1 >> experiment.csv ; \
+#)
+
+
