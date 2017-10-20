7d93bb7564cb9e0c1b96ec2e6075b5ccd25abb79
Andrew Santosa <dcsandr@nus.edu.sg>
2017-10-20 16:09:45 +0800

    Removed scalability/motivating1.c and added scalability/Regexp-7-7.c

--- utils/tacas18-scalability.sh
+++ utils/tacas18-scalability.sh
@@ -12,7 +12,7 @@ source $SCRIPT_DIR/../environ.sh
 unset C_INCLUDE_PATH
 unset CPLUS_INCLUDE_PATH
 
-export EXPERIMENT_SET="malloc9 malloc18 malloc27 motivating1 motivating2 motivating3 Regexp-7-4 Regexp-7-5 Regexp-7-6 insertsort7 insertsort9 insertsort11 bubble_assert3 bubble_assert6 bubble_assert9 nsichneu statemate"
+export EXPERIMENT_SET="malloc9 malloc18 malloc27 motivating2 motivating3 motivating4 Regexp-7-5 Regexp-7-6 Regexp-7-7 insertsort7 insertsort9 insertsort11 bubble_assert3 bubble_assert6 bubble_assert9 nsichneu statemate"
 export EXPERIMENT_TYPE_SET=".klee1 .tx1"
 
 ( \
