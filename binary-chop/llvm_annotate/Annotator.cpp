55587621aed66b26a81624a3143643b9bb4b4717
rasoolmaghareh <rasool@comp.nus.edu.sg>
2019-10-04 17:06:09 +0800

    Adding SV-Comp benchmarks

--- binary-chop/llvm_annotate/Annotator.cpp
+++ binary-chop/llvm_annotate/Annotator.cpp
@@ -2,22 +2,22 @@
 // Copyright 2017 National University of Singapore
 // See LICENSE.md for the license information.
 
-#include "llvm/InitializePasses.h"
-#include "llvm/Pass.h"
-#include "llvm/PassManager.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/ScalarEvolutionExpressions.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/Instruction.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/GlobalValue.h"
 #include "llvm/IRReader/IRReader.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/InitializePasses.h"
+#include "llvm/Pass.h"
+#include "llvm/PassManager.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include <set>
 #include <vector>
