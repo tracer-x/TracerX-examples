2f754feb7a83308d3941496b0caf62375a38143a
rasoolmaghareh <rasool@comp.nus.edu.sg>
2020-06-06 10:41:01 +0800

    updating half_speculation.c

--- basic/half_speculation.c
+++ basic/half_speculation.c
@@ -3,8 +3,13 @@ Copyright 2020 National University of Singapore
 Execute:  make memo_check.tx
 
 Checking the functionality of the special functions tracerx_half_speculation.
+The output is as follows:
 
-These functions implement memo operation. 
+Half Speculation set to:1
+We reached here.
+Half Speculation set to:0
+We reached here.
+
 */
 
 #include <klee/klee.h>
@@ -14,9 +19,9 @@ int main() {
   klee_make_symbolic(&x, sizeof(x), "x");
   
   if (x < 0){
-      tracerx_half_speculation(1);
-  } else {
       tracerx_half_speculation(0);
+  } else {
+      tracerx_half_speculation(1);
   }
   printf("We reached here.\n");
 } 
